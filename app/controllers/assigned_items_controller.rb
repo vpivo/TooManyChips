b6f7254553fdf40a8e3564ab8ec6f62cce49b854
Amy Lukima <amylukima@gmail.com>
2013-07-08 20:17:14 -0700

    move users show into partials

--- app/controllers/assigned_items_controller.rb
+++ app/controllers/assigned_items_controller.rb
@@ -1,14 +1,20 @@
 class AssignedItemsController < ApplicationController
 
-  def create
-  
-    assigned_item = current_user.assigned_items.build(params[:assigned_item])
-    if assigned_item.save
-      redirect_to user_path(current_user)
-    end
-  end
+	def create
+		assigned_item = current_user.assigned_items.build(params[:assigned_item])
+		if assigned_item.save
+			redirect_to user_path(current_user)
+		end
+	end
 
-
+	def destroy
+		@item = AssignedItem.find(params[:format])
+		@item.destroy
+		respond_to do |format|
+			format.html { redirect_to user_path(current_user) }
+			format.xml  { head :ok }
+		end
+	end
 
 end
 
