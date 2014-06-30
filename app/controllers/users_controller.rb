9aea9e308060169c55188c82ffd5341afdc9ac2c
Amy Pivo <amylukima@gmail.com>
2014-06-29 21:47:07 -0700

    basic refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,15 +51,14 @@ class UsersController < ApplicationController
 
   def guest
     @user = User.find_by_url(params[:url])
-    render 'show'
+    session[:id] = @user.id
+    render :js => "window.location = '/guest/#{@user.url}"
   end
 
   private
 
   def add_items(items)
     items.each do |e|
-      puts "looping"
-      puts e
       @user.assigned_items << AssignedItem.new(quantity_provided: e[:amountToBring], event_item_id: e[:id])
       @user.save!
     end
