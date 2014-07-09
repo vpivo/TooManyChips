6a2e287bac59de79a5046eb35aa4619fa1a17220
Amy Pivo <amylukima@gmail.com>
2014-07-09 14:16:36 -0700

    remove migration method

--- db/migrate/20140516195407_add_event_id_to_assigned_items.rb
+++ db/migrate/20140516195407_add_event_id_to_assigned_items.rb
@@ -1,10 +1,5 @@
 class AddEventIdToAssignedItems < ActiveRecord::Migration
   def change
     add_column :assigned_items, :event_id, :integer
-
-    AssignedItem.all.each do |i| 
-      i.event_id = i.event_item.event.id if i.event_item.event.id
-      i.save!
-    end
   end
 end
