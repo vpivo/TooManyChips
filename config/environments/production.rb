cfc91734d466a199c96082f8ed2583fa399873c3
Amy Pivo <amylukima@gmail.com>
2014-07-09 22:40:12 -0700

    clean and compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,15 +9,17 @@ Toomanychips::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+ # config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.js_compress = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+ # config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -64,8 +66,8 @@ config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
     :bucket => 'toomanyc',
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
