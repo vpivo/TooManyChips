bb90ede702cb08a77e5901a23b8e7ac6f0dce3d6
Amy Pivo <amylukima@gmail.com>
2014-07-09 17:06:50 -0700

    fix photo upload error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -58,8 +58,10 @@ class EventsController < ApplicationController
     event.update_items(event_params[:items], event.id) if event_params[:items]
     event.delete_items(event_params[:deletedItems])
     data = event_params.delete([:deletedItems])
+    p data
     event.update_attributes(data)
     event.save!
+    p event.errors.full_messages
     render json: event 
   end
 
